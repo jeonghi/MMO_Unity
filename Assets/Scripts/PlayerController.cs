8f1fb6f3e9a54d442dbbc1cb26562c2de339e7bf
jeonghi <jpark0902@kookmin.ac.kr>
2022-07-24 15:16:48 +0900

    수정 : 플레이어 이동 기준을 월드 좌표계 -> 로컬 좌표계로 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,24 +15,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Local -> World
+        // TransformDirection
+        
+        // World -> Local
+        // InverseTransformDirection
+        
         if (Input.GetKey(KeyCode.W))
         {
             //transform.position += new Vector3(0.0f,  0.0f, 1.0f) * Time.deltaTime * _speed;
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
+            //transform.position += Vector3.forward * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
             //transform.position -= new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * _speed;
-            transform.position += Vector3.back * Time.deltaTime * _speed;
+            //transform.position += Vector3.back * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.A))
         {
             //transform.position -= new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
-            transform.position += Vector3.left * Time.deltaTime * _speed;
+            //transform.position += Vector3.left * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * Time.deltaTime * _speed;
+            //transform.position += Vector3.right * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
         }
     }
 }
